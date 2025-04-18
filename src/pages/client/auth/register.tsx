1eea77a22964cf834fb5e3bf7f38473bbd3dd063
vinhbgdm <ninhdinhvinh11012001@gmail.com>
2025-04-18 11:30:30 +0700

    form register

--- src/pages/client/auth/register.tsx
+++ src/pages/client/auth/register.tsx
@@ -1,6 +1,91 @@
+import { Button, Divider, Form, FormProps, Input } from "antd";
+import { useState } from "react"
+import "./register.scss"
+
+type FieldType = {
+    fullName: string;
+    email: string;
+    password: string;
+    phone: string;
+};
+
 const Register = () => {
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+        console.log('Success:', values);
+    };
+
     return (
-        <div className="">Register page</div>
+        <div className="register-page">
+            <main className="main">
+                <div className="container">
+                    <section className="wrapper">
+                        <div className="heading">
+                            <h2 className="text text-large">Đăng ký tài khoản</h2>
+                            <Divider />
+                        </div>
+                        <Form
+                            name="form-register"
+                            onFinish={onFinish}
+                            autoComplete="off"
+                        >
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }}
+                                label="Họ tên"
+                                name="fullName"
+                                rules={[{ required: true, message: 'Họ tên không được để trống !' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }}
+                                label="Email"
+                                name="email"
+                                rules={[
+                                    { required: true, message: 'Email không được để trống !' },
+                                    { type: "email", message: 'Email không đúng định dạng !' }
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }}
+                                label="Mật khẩu"
+                                name="password"
+                                rules={[{ required: true, message: 'Mật khẩu không được để trống !' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }}
+                                label="Số điện thoại"
+                                name="phone"
+                                rules={[{ required: true, message: 'Số điện thoại không được để trống !' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" loading={isSubmit}>
+                                    Đăng kí
+                                </Button>
+                            </Form.Item>
+                            <Divider>Or</Divider>
+                            <p className="text text-normal" style={{ textAlign: "center" }}>
+                                Đã có tài khoản ?
+                                <a className="primary" style={{ color: "#1677ff", margin: "5px" }}>
+                                    Đăng nhập
+                                </a>
+                            </p>
+                        </Form>
+                    </section>
+                </div>
+            </main>
+        </div>
     )
 }
 
